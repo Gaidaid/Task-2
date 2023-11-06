fea55930b686a51f31681335d024860cbc02e692
Gaidaid <144957660+Gaidaid@users.noreply.github.com>
2023-11-07 01:25:27 +0200

    Add files via upload

---
+++ task-2.js
@@ -0,0 +1,15 @@
+let a = 1
+let b = 'UKD'
+let c = null
+let d = true
+let e = "undefined"
+
+let me = {
+    name:"tea",
+    surname: "iskra",
+    age: 18,
+    home: "Ivano-Frankivsk",
+    Country: "Ukraine"
+}
+
+console.log (a,b,c,d,e, me)
